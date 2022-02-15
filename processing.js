4f30febba14ac491289b57f7efa4b6b3c7379ee9
gwonmin <didrnjsals@gmail.com>
2022-02-15 14:55:30 +0900

    js export import

---
+++ processing.js
@@ -0,0 +1,11 @@
+// import { grade } from './grade.js';
+
+//js : import / export
+//Node.js : require / exports
+
+const grade = require('./grade.js');
+console.log(grade);
+console.log(grade.eng);
+console.log(grade.kor);
+console.log(grade.mat);
+console.log(grade.myfoo());
