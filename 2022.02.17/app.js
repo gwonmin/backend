e1a9bd588d2f4165854df2e96e0103d29ff3c083
gwonmin <didrnjsals@gmail.com>
2022-02-17 13:21:26 +0900

    pick random food

--- 2022.02.17/app.js
+++ 2022.02.17/app.js
@@ -5,13 +5,27 @@ const app = express();
 
 const indexRoute = require('./routes/index.js');
 
-app.use('/', indexRoute)
 
+
+const pickMyFood = () => {
+    const food = ['돈까스', '김밥', '초밥', '샐러드', '햄버거', '피자', '파스타','삼겹살', '순댓국', '치킨', '짜장면', "짬뽕", '오므라이스'];
+    let idx = Math.floor(Math.random() * food.length);
+    
+    return food[idx];
+}
+
+
+app.use('/', indexRoute)
 // req: resquest(요청), 사용자의 브라우저 정보, 질문(주소창), 로그인 정보
 // res: response(응답), 사이트 내용 html
 // app.get('/', (req, res)=> {
 //     res.sendFile(path.join(__dirname, '/index.html'));
 // });
+
+app.get('/food', (req,res)=>{
+    let text = pickMyFood();
+    res.send(`오늘 당신에게 추천드리는 메뉴는 바로 ${text} 입니다.`)
+})
 
 app.get('/webtoon', (req,res)=> {
     res.send("<h2>현재 준비중입니다.ㅜ 조금만 기다려 주세요.</h2>");
