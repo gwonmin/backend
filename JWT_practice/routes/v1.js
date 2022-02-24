9df80932897d5778cb47ec21cc7d13a270a5646d
gwonmin <didrnjsals@gmail.com>
2022-02-24 13:36:00 +0900

    JWT토큰 사용

---
+++ JWT_practice/routes/v1.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const { verifyToken } = require('./middleware.js')
+
+const router = express.Router();
+
+router.get('/token', async(req, res)=> {
+    try {
+        const token = jwt.sign({
+            id: "내 SNS사용자",
+            nick: "human2",
+            grade: "premium"
+        }, process.env.JWT_SECRET, {
+            expiresIn: '60m',
+            issuer: 'mySNS',
+        })
+        req.session.jwt = token;
+        return res.json({
+            code: 200,
+            message: '토큰이 발급되었습니다.',
+            token
+        });
+
+    } catch(err) {
+        console.error(err);
+        return res.status(500).json({
+            code: 500,
+            message: '서버 에러'
+        })
+    }
+})
+
+router.get('/test', verifyToken, (req, res)=> {
+    res.json(req.decoded)
+})
+
+module.exports = router;
